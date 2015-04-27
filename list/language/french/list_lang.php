4322aa10b70a18fe3f11b9eb36628c54a937d325
Mamarmite <support@mamarmite.com>
2015-04-27 16:24:18 -0400

    Add French and labels to localization

--- list/language/french/list_lang.php
+++ list/language/french/list_lang.php
@@ -1,9 +1,9 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed');
 
 // labels
-$lang['streams:list.name']                          = "List";
+$lang['streams:list.name']                          = "Liste";
 $lang['streams:list.return_type']                   = "";
 $lang['streams:list.return_type.instructions']      = "";
-$lang['streams:list.placeholder']					= "List item content...";
+$lang['streams:list.placeholder']					= "Insérez un élément de votre liste ici.";
 $lang['streams:list.item_actions.add']				= "+";
 $lang['streams:list.item_actions.remove']			= "-";
